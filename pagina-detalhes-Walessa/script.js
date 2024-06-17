f020b1ad89300b33d60ed0440836e83044963676
lszlsvgcon <159768697+lszlsvgcon@users.noreply.github.com>
2024-06-17 18:39:06 -0300

    atualizacao de pagina detalhes e adicionado arquivo javascript

---
+++ pagina-detalhes-Walessa/script.js
@@ -0,0 +1,32 @@
+// Modificando o nome do som para azul quando clicado
+document.getElementById("soundName").addEventListener("click", function () {
+  this.style.color = "blue";
+});
+
+// Renomeando os botões e alterando suas funções
+var playButton = document.getElementById("playButton");
+var pauseButton = document.getElementById("pauseButton");
+var stopButton = document.getElementById("stopButton");
+
+playButton.addEventListener("click", playMusic);
+pauseButton.addEventListener("click", pauseMusic);
+stopButton.addEventListener("click", stopMusic);
+
+// Adicionando função playMusic
+function playMusic() {
+  var audio = document.getElementById("som");
+  audio.play();
+}
+
+// Adicionando função pauseMusic
+function pauseMusic() {
+  var audio = document.getElementById("som");
+  audio.pause();
+}
+
+// Adicionando função stopMusic
+function stopMusic() {
+  var audio = document.getElementById("som");
+  audio.pause();
+  audio.currentTime = 0;
+}
