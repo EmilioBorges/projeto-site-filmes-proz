04b452133b5d28a7fc2ba836ef123ad9b52acba7
lszlsvgcon <lsfmaillsvl@gmail.com>
2024-06-18 18:30:44 -0300

    alteracao no header e acrescimo do scroll com javascript

--- pagina-detalhes-Walessa/script.js
+++ pagina-detalhes-Walessa/script.js
@@ -30,3 +30,17 @@ function stopMusic() {
   audio.pause();
   audio.currentTime = 0;
 }
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const elements = document.querySelectorAll(".hidden");
+
+elements.forEach((element) => myObserver.observe(element));
